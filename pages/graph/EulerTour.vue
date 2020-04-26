a93083cc666f119ad01410580dbd1b792da5702b
jellyfish26 <jellyfish0162@gmail.com>
2020-04-27 00:10:27 +0900

    Add EulerTour page

---
+++ pages/graph/EulerTour.vue
@@ -0,0 +1,29 @@
+<template>
+  <LibraryPage
+    sourceRaw="https://raw.githubusercontent.com/jellyfish26/competitive-library/master/graph/EulerTour.cpp"
+    sourceGitHubLink="https://github.com/jellyfish26/competitive-library/blob/master/graph/EulerTour.cpp"
+    :dependentList="dependent_list"
+    :japanese="Japanese.ja"
+    :english="English.en"
+  />
+</template>
+
+<script>
+import LibraryPage from '~/components/LibraryPage.vue'
+import DependentList from '~/locales/graph/EulerTour/list.js'
+import Japanese from '~/locales/graph/EulerTour/ja.json'
+import English from '~/locales/graph/EulerTour/en.json'
+
+export default {
+  components: {
+    LibraryPage: LibraryPage
+  },
+  data() {
+    return {
+      Japanese: Japanese,
+      English: English,
+      dependent_list: DependentList.data(),
+    }
+  },
+}
+</script>
