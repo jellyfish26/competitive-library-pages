ae6704971af9c9830b650a1edea6d45d39251197
jellyfish26 <jellyfish0162@gmail.com>
2020-04-27 22:25:15 +0900

    add CountingCharacterIntervalNumericMultiples page :)

--- layouts/navigation-lists.js
+++ layouts/navigation-lists.js
@@ -88,6 +88,10 @@ export default {
               name: 'enumerative_euler_tour',
               to: 'custom-CountUpEulerTour'
             },
+            {
+              name: 'counting_character_interval_mumeric_multiples',
+              to: 'custom-CountingCharacterIntervalNumericMultiples'
+            },
           ]
         }
       ]
