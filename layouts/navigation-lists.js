58638f9d31e41b702194b9314a30158f6200cd95
jellyfish26 <jellyfish0162@gmail.com>
2021-03-05 10:33:42 +0000

    Delete unneccessary pages

--- layouts/navigation-lists.js
+++ layouts/navigation-lists.js
@@ -3,10 +3,6 @@ import graph from './classification_list/graph.js'
 import algorithm from './classification_list/algorithm.js'
 import mathematics from './classification_list/mathematics.js'
 import geometry from './classification_list/geometry.js'
-import extension_graph from './classification_list/custom/extension_graph.js'
-import extension_algorithm from './classification_list/custom/extension_algorithm.js'
-import custom_others from './classification_list/custom/others.js'
-import custom_garbage from './classification_list/custom/garbage.js'
 
 export default {
   data() {
@@ -50,34 +46,6 @@ export default {
           to: '/geometry',
           open: false,
           list: geometry.data().list
-        },
-        {
-          icon: 'mdi-graphql',
-          name: 'extension_graph',
-          to: '/custom',
-          open: false,
-          list: extension_graph.data().list
-        },
-        {
-          icon: 'mdi-cube-unfolded',
-          name: 'extension_algorithm',
-          to: '/custom',
-          open: false,
-          list: extension_algorithm.data().list
-        },
-        {
-          icon: 'mdi-flask-empty-plus',
-          name: 'custom',
-          to: '/custom',
-          open: false,
-          list: custom_others.data().list
-        },
-        {
-          icon: 'mdi-delete',
-          name: 'garbage',
-          to: '/garbage',
-          open: false,
-          list: custom_garbage.data().list
         }
       ]
     }
